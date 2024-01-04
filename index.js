e51957108d6aa70f21467ab694d3316366c2525f
Mindy D Tran <mindytran98@gmail.com>
2024-01-04 18:46:35 -0500

    resetting the change

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const express = require('express');
 
 const app = express();
 const port = 3000;
-app.set('view enging', 'ejs');
+// app.set('view enging', 'ejs');
 
 app.get('/', (req,res)=>{
     console.log(req.url);
+    // res.render('pages/index');
     res.send('hewo')
 })
-app.listen(port, () =>console.log('port is runnung'));
+app.listen(port, () =>console.log('port is running'));
