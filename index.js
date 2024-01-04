42ae28a67121f05f541f2f42ad3174ae7de8bc4c
Mindy D Tran <mindytran98@gmail.com>
2024-01-04 18:30:44 -0500

    set the default page

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const express = require('express');
 
 const app = express();
+const port = 3000;
+app.set('view enging', 'ejs');
+
+app.get('/', (req,res)=>{
+    console.log(req.url);
+    res.send('hewo')
+})
+app.listen(port, () =>console.log('port is runnung'));
