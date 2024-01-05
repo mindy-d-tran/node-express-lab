38aa508144dac0aeaba428a1b1c2bd2463a64a51
Mindy D Tran <mindytran98@gmail.com>
2024-01-05 09:37:08 -0500

    add middleware

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const app = express();
 const port = 3000;
 app.set('view engine', 'ejs');
+// middleware
+app.use(morgan('short'));
 
 app.get('/', (req,res)=>{
     console.log(req.url);
