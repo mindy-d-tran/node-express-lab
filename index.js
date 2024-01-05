ace72a5f118a52005ebff1b205586fe64a755d1a
Mindy D Tran <mindytran98@gmail.com>
2024-01-05 15:09:39 -0500

    correctly redirect after logging in

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
+
+const loginRoute = require('./routes/login')
 const app = express();
 const port = 3000;
 //view engine
@@ -7,17 +9,20 @@ app.set('view engine', 'ejs');
 
 // middleware
 app.use(morgan('short'));
+// app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 app.use(express.static('./styles'));
 // routes
-app.get('/', (req,res)=>{
-    console.log(req.url);
-    res.render('pages/index');
+app.use('/', loginRoute)
+app.get('/welcome', (req,res)=>{
+    res.render('pages/welcome');
 })
-app.get('/login', (req,res)=>{
-    res.render('pages/login')
+app.post('/welcome', (req,res)=>{
+    console.log(req.body);
+    res.send('did it work?')
 })
 // automaticially redirect to the login page if it's not one of the routes above
-app.all('*', (req,res)=>{
-    res.redirect('/login');
-})
+// app.all('*', (req,res)=>{
+//     res.redirect('/login');
+// })
 app.listen(port, () =>console.log(`port is running ${port}`));
