4abc94176d10443cba8474aee4b535b8343758f9
Mindy D Tran <mindytran98@gmail.com>
2024-01-05 14:13:51 -0500

    add all to redirect to login if they try to go to any other page

--- index.js
+++ index.js
@@ -7,9 +7,17 @@ app.set('view engine', 'ejs');
 
 // middleware
 app.use(morgan('short'));
-
+app.use(express.static('./styles'));
+// routes
 app.get('/', (req,res)=>{
     console.log(req.url);
     res.render('pages/index');
+})
+app.get('/login', (req,res)=>{
+    res.render('pages/login')
+})
+// automaticially redirect to the login page if it's not one of the routes above
+app.all('*', (req,res)=>{
+    res.redirect('/login');
 })
 app.listen(port, () =>console.log(`port is running ${port}`));
