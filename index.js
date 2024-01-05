69877068173283a47c8cb39abec7a4022ff796f2
Mindy D Tran <mindytran98@gmail.com>
2024-01-05 17:28:23 -0500

    revert changes to project

--- index.js
+++ index.js
@@ -1,28 +1,26 @@
-const express = require('express');
-const morgan = require('morgan');
+const express = require("express");
+const morgan = require("morgan");
 
-const loginRoute = require('./routes/login')
+//exporting routes
+const loginRoute = require("./routes/login");
+const welcomeRoute = require("./routes/welcome");
+
 const app = express();
 const port = 3000;
 //view engine
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 // middleware
-app.use(morgan('short'));
-// app.use(express.json());
-app.use(express.urlencoded({extended:true}));
-app.use(express.static('./styles'));
+app.use(morgan("short"));
+// store date from form
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("./styles"));
+
 // routes
-app.use('/', loginRoute)
-app.get('/welcome', (req,res)=>{
-    res.render('pages/welcome');
-})
-app.post('/welcome', (req,res)=>{
-    console.log(req.body);
-    res.send('did it work?')
-})
+app.use("/", loginRoute);
+app.use("/welcome", welcomeRoute);
 // automaticially redirect to the login page if it's not one of the routes above
 // app.all('*', (req,res)=>{
 //     res.redirect('/login');
 // })
-app.listen(port, () =>console.log(`port is running ${port}`));
+app.listen(port, () => console.log(`port is running ${port}`));
