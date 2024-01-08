8233728262f8cf3d2f8d6ba47f0e938ba04a2171
Mindy D Tran <mindytran98@gmail.com>
2024-01-08 09:24:30 -0500

    add image to assets foler

--- routes/user.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-//create a router lika a mini app
-const router = express.Router();
-
-router.get('/',(req,res)=>{
-    res.send('getting user');
-})
-
-module.exports = router;
